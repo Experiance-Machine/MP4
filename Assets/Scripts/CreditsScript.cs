938d3662cce32189c3551746572364852c4b4cac
tompgraham <tgraha03@uw.edu>
2016-04-28 00:20:05 -0700

    Credits Script

---
+++ Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour {
+    public Button mReturnButton;
+    // Use this for initialization
+    void Start () {
+        mReturnButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+
+        mReturnButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("Menu");
+            });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LoadScene(string theLevel)
+    {
+        SceneManager.LoadScene(theLevel);
+        //        FirstGameManager.TheGameState.SetCurrentLevel(theLevel);
+
+    }
+}
