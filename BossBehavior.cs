bf75c0796d7fd1ea0abb3b793f638a59a95de3d8
tompgraham <tgraha03@uw.edu>
2016-04-27 23:32:41 -0700

    Transition + Scoring

---
+++ BossBehavior.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BossBehavior : MonoBehaviour
+{
+
+    private const float kReferenceSpeed = 20f;
+
+    public float mSpeed = kReferenceSpeed;
+    public float mTowardsCenter = 0.5f;
+    private static GameObject mAbduct;
+    public Vector3 enemyPortalOffset = new Vector3(0, 3.5f, 0); // Defined in object settings
+    private Vector3 worldPortalOffset; // Multiplied by rotation
+    private SpriteRenderer renderer;
+    private static Sprite spriteDefault;
+    private static Sprite spriteStunned;
+    private static Sprite spriteAfraid;
+    private Vector3 run;
+    private Vector3 mRotation;
+
+    private static GameObject hero;
+    private Vector3 heroDiff;
+    private float heroDot;
+    public float maxScare = 30f;
+
+
+    private static BossBackground globalBehavior;
+    private BossBackground.WorldBoundStatus status;
+
+    private float stunTimer;
+    private int stunCount;
+    public int stunMax = 3;
+    public float stunDuration = 5.0f; // in seconds
+    int timesHit = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (globalBehavior == null)
+        {
+            globalBehavior = GameObject.Find("GameManager").GetComponent<BossBackground>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += (200f * Time.smoothDeltaTime) * transform.up;
+
+        status = globalBehavior.ObjectCollideWorldBound(GetComponent<Renderer>().bounds);
+         if (status != BossBackground.WorldBoundStatus.Inside)
+         {
+             NewDirection();
+         }
+         
+    }
+
+    // New direction will be something randomly within +- 45-degrees away from the direction
+    // towards the center of the world
+    private void NewDirection()
+    {
+
+        // we want to move towards the center of the world
+        Vector2 v = globalBehavior.WorldCenter - new Vector2(transform.position.x, transform.position.y);
+        // this is vector that will take us back to world center
+        v.Normalize();
+        Vector2 vn = new Vector2(v.y, -v.x); // this is a direciotn that is perpendicular to V
+
+        float useV = 1.0f - Mathf.Clamp(mTowardsCenter, 0.01f, 1.0f);
+        float tanSpread = Mathf.Tan(useV * Mathf.PI / 2.0f);
+
+        float randomX = Random.Range(0f, 1f);
+        float yRange = tanSpread * randomX;
+        float randomY = Random.Range(-yRange, yRange);
+
+        Vector2 newDir = randomX * v + randomY * vn;
+        newDir.Normalize();
+        transform.up = newDir;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        // Only care if hitting a Laser (vs. hitting another Enemy!
+        if (other.gameObject.name == "Laser(Clone)")
+        {
+           // worldPortalOffset = other.gameObject.transform.rotation * enemyPortalOffset;
+            //GameObject e = Instantiate(mAbduct, other.gameObject.transform.position + worldPortalOffset, other.gameObject.transform.rotation) as GameObject;
+            Destroy(other.gameObject);
+            timesHit++;
+            Debug.Log("Hit" + timesHit);
+        }
+        if (timesHit > 50) {
+            Destroy(this.gameObject);
+            GlobalBehavior.score += 50;
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
---
+++ GlobalBehavior.cs
@@ -0,0 +1,118 @@
+/* Copyrights:
+ *  Background:     https://www.youtube.com/watch?v=-KLOrIu1RC4
+ *  Alien/Laser:    http://opengameart.org/content/alien-ufo-pack
+ *  Enemies:        http://opengameart.org/content/match-tiles-emoji-asset-public-domain
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GlobalBehavior : MonoBehaviour 
+{
+	
+	#region World Bound support
+	private Bounds mWorldBound;  // this is the world bound
+	public Vector2 mWorldMin;	// Better support 2D interactions
+	public Vector2 mWorldMax;
+	public Vector2 mWorldCenter;
+	private Camera mMainCamera;
+	#endregion
+
+    public Text echoText;
+    public static int score;
+
+    //private bool toggleFrozen;
+
+	// Use this for initialization
+	void Start () 
+    {
+        DontDestroyOnLoad(this);
+
+		#region world bound support
+		mMainCamera = Camera.main;
+		mWorldBound = new Bounds(Vector3.zero, Vector3.one);
+		UpdateWorldWindowBound();
+		#endregion
+
+        echoText.text = "";
+        //toggleFrozen = false;
+	}
+
+	// Update is called once per frame
+	void Update () 
+    {
+        SetEchoText();
+	}
+	
+    public void SetEchoText()
+    {
+        echoText.text = "Score: " + score;
+    }
+
+	#region Game Window World size bound support
+	public enum WorldBoundStatus 
+    {
+		CollideTop,
+		CollideLeft,
+		CollideRight,
+		CollideBottom,
+		Outside,
+		Inside
+	};
+	
+	/// <summary>
+	/// This function must be called anytime the MainCamera is moved, or changed in size
+	/// </summary>
+	public void UpdateWorldWindowBound()
+	{
+		// get the main 
+		if (null != mMainCamera) {
+			float maxY = mMainCamera.orthographicSize;
+			float maxX = mMainCamera.orthographicSize * mMainCamera.aspect;
+			float sizeX = 2 * maxX;
+			float sizeY = 2 * maxY;
+			float sizeZ = Mathf.Abs(mMainCamera.farClipPlane - mMainCamera.nearClipPlane);
+			
+			// Make sure z-component is always zero
+			Vector3 c = mMainCamera.transform.position;
+			c.z = 0.0f;
+			mWorldBound.center = c;
+			mWorldBound.size = new Vector3(sizeX, sizeY, sizeZ);
+
+			mWorldCenter = new Vector2(c.x, c.y);
+			mWorldMin = new Vector2(mWorldBound.min.x, mWorldBound.min.y);
+			mWorldMax = new Vector2(mWorldBound.max.x, mWorldBound.max.y);
+		}
+	}
+	
+	public Vector2 WorldCenter { get { return mWorldCenter; } }
+	public Vector2 WorldMin { get { return mWorldMin; }} 
+	public Vector2 WorldMax { get { return mWorldMax; }}
+	
+	public WorldBoundStatus ObjectCollideWorldBound(Bounds objBound)
+	{
+		WorldBoundStatus status = WorldBoundStatus.Inside;
+		
+		if (mWorldBound.Intersects(objBound)) {
+			if (objBound.max.x > mWorldBound.max.x)
+				status = WorldBoundStatus.CollideRight;
+			else if (objBound.min.x < mWorldBound.min.x)
+				status = WorldBoundStatus.CollideLeft;
+			else if (objBound.max.y > mWorldBound.max.y)
+				status = WorldBoundStatus.CollideTop;
+			else if (objBound.min.y < mWorldBound.min.y)
+				status = WorldBoundStatus.CollideBottom;
+			else if ( (objBound.min.z < mWorldBound.min.z) || (objBound.max.z > mWorldBound.max.z))
+				status = WorldBoundStatus.Outside;
+		} else 
+			status = WorldBoundStatus.Outside;
+		return status;
+		
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 150, 30), "Score: " + score);
+    }
+    #endregion
+}
